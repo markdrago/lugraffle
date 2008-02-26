f29112ee48351a591bde38400f16f0626f5b746d
Mark Drago <markdrago@gmail.com>
2008-02-26 07:54:32 -0500

    move the network stuff from lugraffle.py to lr_net.py

---
+++ src/lr_net.py
@@ -0,0 +1,52 @@
+from socket import *
+from twisted.internet.protocol import DatagramProtocol
+from protocol import *
+import utils
+import logging
+
+#network interface for lug raffle
+class LRServer(DatagramProtocol):
+    def __init__(self, port, model, reactor):
+	self.port = port
+	self.model = model
+	self.logger = logging.getLogger('LR.LRServer')
+	self.model.register_listener('net', self.announce_change, False)
+	reactor.listenUDP(self.port, self)
+
+    def startProtocol(self):
+	self.sendsocket = socket(AF_INET, SOCK_DGRAM)
+	self.sendsocket.bind(('', 0))
+	self.sendsocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+	self.local_ip_list = utils.get_local_ipv4_addresses()
+
+    def datagramReceived(self, data, (host, port)):
+	#we should not handle packets that we sent ourselves
+	if host in self.local_ip_list:
+	    self.logger.debug("Not replying to packet from self")
+	    return
+	
+	self.logger.info("Received packet from %s:%d" % (host, port))
+	try:
+	    packet = LRPacket(data)
+	except LRPacketError, (errstr):
+	    self.logger.warning("Error while parsing packet: %s" % errstr)
+	    return
+	    
+	#packet is changing things, tell the model about it
+	if (packet.packet_type == 'RAFFLE_NODE_FOUND' or
+	    packet.packet_type == 'RAFFLE_OBJECT_ADD'):
+	    for item in packet.items:
+		self.model.add_item('net', item)
+	    for entry in packet.entries:
+		self.model.add_entry('net', entry[0], entry[1])
+
+	self.model.dump_model_state()
+
+    def announce_change(self, item, entry):
+	packet = LRPacket()
+	packet.set_type('RAFFLE_OBJECT_ADD')
+	packet.add_object(item, entry)
+	data = packet.produce_packet()
+
+	self.logger.info("Sending Data: %s" % data)
+	self.sendsocket.sendto(data, ('<broadcast>', self.port))
--- src/lugraffle.py
+++ src/lugraffle.py
@@ -3,8 +3,6 @@
 #Copyright (c) 2007, 2008 Mark Drago <markdrago@gmail.com>
 
 import logging, sys
-from twisted.internet.protocol import DatagramProtocol
-from socket import *
 
 #import reactor stuff, tell it to use the glib2 main loop for dbus support
 from twisted.internet import glib2reactor
@@ -12,9 +10,8 @@ glib2reactor.install()
 from twisted.internet import reactor
 
 #import our stuff
-import utils
-from protocol import *
 from lr_model import *
+from lr_net import *
 
 class LugRaffle():
     def __init__(self):
@@ -24,57 +21,10 @@ class LugRaffle():
 			    stream=sys.stderr)
     	self.logger = logging.getLogger('LR.LugRaffle')
 	self.model = LRModel()
-	self.server = LRServer(1234, self.model)
+	self.server = LRServer(1234, self.model, reactor)
 
     def main(self):
 	reactor.run()
-
-#network interface for lug raffle
-class LRServer(DatagramProtocol):
-    def __init__(self, port, model):
-	self.port = port
-	self.model = model
-	self.logger = logging.getLogger('LR.LRServer')
-	self.model.register_listener('net', self.announce_change, False)
-	reactor.listenUDP(self.port, self)
-
-    def startProtocol(self):
-	self.sendsocket = socket(AF_INET, SOCK_DGRAM)
-	self.sendsocket.bind(('', 0))
-	self.sendsocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-	self.local_ip_list = utils.get_local_ipv4_addresses()
-
-    def datagramReceived(self, data, (host, port)):
-	#we should not handle packets that we sent ourselves
-	if host in self.local_ip_list:
-	    self.logger.debug("Not replying to packet from self")
-	    return
-	
-	self.logger.info("Received packet from %s:%d" % (host, port))
-	try:
-	    packet = LRPacket(data)
-	except LRPacketError, (errstr):
-	    self.logger.warning("Error while parsing packet: %s" % errstr)
-	    return
-	    
-	#packet is changing things, tell the model about it
-	if (packet.packet_type == 'RAFFLE_NODE_FOUND' or
-	    packet.packet_type == 'RAFFLE_OBJECT_ADD'):
-	    for item in packet.items:
-		self.model.add_item('net', item)
-	    for entry in packet.entries:
-		self.model.add_entry('net', entry[0], entry[1])
-
-	self.model.dump_model_state()
-
-    def announce_change(self, item, entry):
-	packet = LRPacket()
-	packet.set_type('RAFFLE_OBJECT_ADD')
-	packet.add_object(item, entry)
-	data = packet.produce_packet()
-
-	self.logger.info("Sending Data: %s" % data)
-	self.sendsocket.sendto(data, ('<broadcast>', self.port))
 
 #start listening for connections on our udp port
 if __name__ == '__main__':
