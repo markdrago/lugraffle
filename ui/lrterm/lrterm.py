8d66478f5a898c948bada6be97fb7c5154e71c2f
Mark Drago <markdrago@gmail.com>
2008-03-10 08:41:24 -0400

    add ability to alter model through lrterm

--- ui/lrterm/lrterm.py
+++ ui/lrterm/lrterm.py
@@ -1,4 +1,4 @@
-import logging, sys, readline
+import logging, sys, readline, shlex
 import dbus
 
 class LRTerm():
@@ -8,44 +8,56 @@ class LRTerm():
 	self.dbus_obj = self.dbus_bus.get_object('org.lilug.lugraffle', '/')
 	self.dbus_iface = dbus.Interface(self.dbus_obj,
 					 dbus_interface='org.lilug.lugraffle')
-	self.cmds = [["ls", [0, 1], self.ls,
+	self.cmds = {"ls" : [ [0, 1], self.ls,
 		      "[<item>] list all items or entries for an item"],
-#		     ["touch", [1], self.touch,
-#		      "[item/entry] add entry to item"],
-#		     ["mkdir", [1], self.mkdir,
-#		      "[item] add item"],
-		     ["help", [0], self.help, "show this help output"],
-		     ["exit", [0], self.exit, "exit"],
-		     ["q", [0], self.exit, "exit"]]
+		     "touch" : [ [1], self.touch,
+		      "[item/entry] add entry to item"],
+		     "mkdir" : [ [1], self.mkdir, "[item] add item"],
+		     "help" : [ [0], self.help, "show this help output"],
+		     "exit" : [ [0], self.exit, "exit"],
+		     "q" : [ [0], self.exit, "exit"]}
 
     def main(self):
 	while 1:
 	    (cmd, args) = self.get_command()
-	    if cmd == "ls":
-		self.print_items(None)
-	    elif cmd == "help":
-		self.help()
-	    elif cmd == "exit" or cmd == "q":
-		self.exit()
-#	    elif cmd == "ls items":
-#		self.print_items()
+	    self.cmds[cmd][1](args)
 
-
-    def exit(self):
+    def exit(self, args):
 	exit(0)
 
-    def ls(self, item):
-	if item is None:
+    def ls(self, args):
+	if args is None:
+	    self.print_items()
+	elif args[0] == '*':
 	    self.print_items_and_entries()
 	else:
-	    self.print_items()
+	    self.print_entries_for_item(args[0])
 
-    def help(self):
+    def touch(self, args):
+	parts = args[0].split('/')
+	if len(parts) != 2:
+	    return
+	self.add_entry(parts[0], parts[1])
+
+    def mkdir(self, args):
+	self.add_item(args[0])
+
+    def help(self, args):
 	print "Commands:"
 	for tpl in self.cmds:
 	    print "%s\t-- %s" % (tpl[0], tpl[3])
 
-    def print_items():
+    def add_item(self, item):
+	self.dbus_iface.add_item(item)
+
+    def add_entry(self, item, entry):
+	self.dbus_iface.add_entry(item, entry)
+
+    def print_entries_for_item(self, item):
+	for entry in self.dbus_iface.get_entries_for_item(item):
+	    print "%s" % entry
+
+    def print_items(self):
 	for item in self.dbus_iface.get_items():
 	    print "%s" % item
 
@@ -60,17 +72,24 @@ class LRTerm():
 	    print ""
 
     def get_command(self):
-	#get list of all legal commands
-	allowed_cmds = []
-	for tpl in self.cmds:
-	    allowed_cmds.append(tpl[0])
-
 	#get a legal command
 	cmd = ""
-	while cmd not in allowed_cmds:
-	    cmd = raw_input("lr> ")
+	while cmd not in self.cmds.keys():
+	    line = raw_input("lr> ")
+	    pieces = shlex.split(line)
+	    if len(pieces) > 0:
+		cmd = pieces[0]
 
-	return (cmd, None)
+	#make sure we have a legal # of arguments for this command
+	if (len(pieces) - 1) in self.cmds[cmd][0]:
+	    if len(pieces) > 1:
+		args = pieces[1:]
+	    else:
+		args = None
+	else:
+	    return self.get_command()
+
+	return (cmd, args)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
