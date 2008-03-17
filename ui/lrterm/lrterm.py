b5e0dd0f51fc7166c2e5ebf806a7ac53339b869c
Justin Azoff <justin@bouncybouncy.net>
2008-03-17 19:35:29 -0400

    rewrite lrterm to use Cmd python module

--- ui/lrterm/lrterm.py
+++ ui/lrterm/lrterm.py
@@ -2,11 +2,14 @@
 #Licensed under the MIT license
 #Copyright (c) 2008 Mark Drago <markdrago@gmail.com>
 
-import logging, sys, readline, shlex
+import logging, sys
 import dbus
+from cmd import Cmd
 
-class LRTerm():
+class LRTerm(Cmd):
+    prompt = 'lr> '
     def __init__(self):
+	Cmd.__init__(self)
 	self.logger = logging.getLogger('LRTerm')
 	self.dbus_bus = dbus.SessionBus()
 	try:
@@ -17,44 +20,43 @@ class LRTerm():
 	    exit(1)
 	self.dbus_iface = dbus.Interface(self.dbus_obj,
 					 dbus_interface='org.lilug.lugraffle')
-	self.cmds = {"ls" : [ [0, 1], self.ls,
-		      "[<item>] list all items or entries for an item"],
-		     "touch" : [ [1], self.touch,
-		      "[item/entry] add entry to item"],
-		     "mkdir" : [ [1], self.mkdir, "[item] add item"],
-		     "help" : [ [0], self.help, "show this help output"],
-		     "exit" : [ [0], self.exit, "exit"],
-		     "q" : [ [0], self.exit, "exit"]}
 
-    def main(self):
-	while 1:
-	    (cmd, args) = self.get_command()
-	    self.cmds[cmd][1](args)
-
-    def exit(self, args):
+    def exit(self, args=None):
 	exit(0)
 
-    def ls(self, args):
-	if args is None:
+    def do_EOF(self, x):
+	"""Exit"""
+	print
+	print "Goodbye!"
+	self.exit()
+
+    def do_ls(self, item):
+	"""ls [<item>]
+	list all items or entries for an item"""
+	if not item:
 	    self.print_items()
-	elif args[0] == '*':
+	elif item == '*':
 	    self.print_items_and_entries()
 	else:
-	    self.print_entries_for_item(args[0])
+	    self.print_entries_for_item(item)
+ 
+    def complete_ls(self, text, line, begidx, endidx):
+	return [i for i in self.dbus_iface.get_items() if i.startswith(text)]
 
-    def touch(self, args):
-	parts = args[0].split('/')
+    complete_touch = complete_ls
+  
+    def do_touch(self, arg):
+	"""touch [item/entry]
+	add entry to item"""
+	parts = arg.split('/')
 	if len(parts) != 2:
 	    return
 	self.add_entry(parts[0], parts[1])
-
-    def mkdir(self, args):
-	self.add_item(args[0])
-
-    def help(self, args):
-	print "Commands:"
-	for cmd in self.cmds.keys():
-	    print "%s\t-- %s" % (cmd, self.cmds[cmd][2])
+  
+    def do_mkdir(self, item):
+	"""mkdir item
+	add item"""
+	self.add_item(item)
 
     def add_item(self, item):
 	self.dbus_iface.add_item(item)
@@ -80,26 +82,6 @@ class LRTerm():
 		sep = ", "
 	    print ""
 
-    def get_command(self):
-	#get a legal command
-	cmd = ""
-	while cmd not in self.cmds.keys():
-	    line = raw_input("lr> ")
-	    pieces = shlex.split(line)
-	    if len(pieces) > 0:
-		cmd = pieces[0]
-
-	#make sure we have a legal # of arguments for this command
-	if (len(pieces) - 1) in self.cmds[cmd][0]:
-	    if len(pieces) > 1:
-		args = pieces[1:]
-	    else:
-		args = None
-	else:
-	    return self.get_command()
-
-	return (cmd, args)
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
 			format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -107,4 +89,4 @@ if __name__ == '__main__':
 			stream=sys.stderr)
 
     lrterm = LRTerm()
-    lrterm.main()
+    lrterm.cmdloop()
