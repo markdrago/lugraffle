ac6b5c2598194aba8031f989faeac337de746d1a
Mark Drago <markdrago@gmail.com>
2008-04-28 21:39:27 -0400

    largeish commit that improves the lrgtk list a bit, still can not add items

--- ui/lrgtk/lrgtk.py
+++ ui/lrgtk/lrgtk.py
@@ -18,28 +18,22 @@ except:
 class LRGtk:
     def __init__(self):
         self.logger = logging.getLogger('LRGtk')
-        self.init_dbus()
         self.init_gtk()
+        self.list = LRGtk_List(self.tree_view)
+        self.init_dbus()
+        self.init_tree()
+        self.main_window.show()
 
     def init_gtk(self):
-        #load glade file
         gladefile = 'lrgtk.glade'
-        self.gladedoc = gtk.glade.XML(gladefile, 'main_window')
-        self.gladedoc.signal_autoconnect(self)
-
-        #add event to close window
-        self.main_window = self.gladedoc.get_widget('main_window')
-        self.main_window.connect("delete_event", self.exit)
-
-        #create treestore
-        self.tree_store = gtk.TreeStore(str)
-        tree_view = self.gladedoc.get_widget('model_tree_view')
-        tree_view.set_model(self.tree_store)
-        tree_view.set_headers_visible(False)
-        renderer = gtk.CellRendererText()
-        column = gtk.TreeViewColumn("Name", renderer, text=0)
-        tree_view.append_column(column)
-        self.init_tree()
+        self.main_win = gtk.glade.XML(gladefile, 'main_window')
+        self.main_win.signal_autoconnect(self)
+        self.main_window = self.main_win.get_widget('main_window')
+        self.tree_view = self.main_win.get_widget('model_tree_view')
+        self.add_item_win = gtk.glade.XML(gladefile, 'add_item_dialog')
+        self.add_item_win.signal_autoconnect(self)
+        self.add_item_dialog = self.add_item_win.get_widget('add_item_dialog')
+        self.add_item_dialog.connect("delete_event", self.hide_add_item_dialog)
 
     def init_dbus(self):
         self.dbus_bus = dbus.SessionBus()
@@ -51,21 +45,49 @@ class LRGtk:
             exit(1)
         self.dbus_iface = dbus.Interface(self.dbus_obj,
                                          dbus_interface='org.lilug.lugraffle')
-        self.dbus_iface.connect_to_signal('item_added', self.item_added)
-        self.dbus_iface.connect_to_signal('entry_added', self.entry_added)
+        self.dbus_iface.connect_to_signal('item_added', self.list.add_item)
+        self.dbus_iface.connect_to_signal('entry_added', self.list.add_entry)
 
-    def main(self):
-        self.main_window.show()
-        gtk.main()
+    def show_add_item_dialog(self, event):
+        self.add_item_dialog.show()
 
-    def item_added(self, item):
+    def hide_add_item_dialog(self, widget, event):
+        print "Hiding add item dialog"
+        self.add_item_dialog.hide()
+
+    def add_item(self, item):
+        self.list.add_item(item)
+
+    def init_tree(self):
+        model = self.dbus_iface.get_items_and_entries()
+        for item in model.keys():
+            self.list.add_item(item)
+            for entry in model[item]:
+                self.list.add_entry(item, entry)
+
+    def exit(self, widget, event):
+        gtk.main_quit()
+        exit(0)
+
+class LRGtk_List:
+    def __init__(self, tree_view):
+        #create treestore
+        self.tree_view = tree_view
+        self.tree_store = gtk.TreeStore(str)
+        self.tree_view.set_model(self.tree_store)
+        self.tree_view.set_headers_visible(False)
+        renderer = gtk.CellRendererText()
+        column = gtk.TreeViewColumn("Name", renderer, text=0)
+        self.tree_view.append_column(column)
+
+    def add_item(self, item):
         item_iter = self.find_item(item)
         if item_iter is not None:
             self.logger.debug("Item already exists: %s" % item)
             return
         self.tree_store.append(None, (item,))
 
-    def entry_added(self, item, entry):
+    def add_entry(self, item, entry):
         item_iter = self.find_item(item)
         if item_iter is None:
             self.logger.debug("No item found with name: %s" % item)
@@ -76,23 +98,12 @@ class LRGtk:
             return
         self.tree_store.append(item_iter, (entry,))
 
-    def init_tree(self):
-        model = self.dbus_iface.get_items_and_entries()
-        for item in model.keys():
-            self.item_added(item)
-            for entry in model[item]:
-                self.entry_added(item, entry)
-
     def find_entry(self, item_iter, entry):
         entry_iter = self.tree_store.iter_children(item_iter)
-        while True:
-            if entry_iter is None:
-                break
-            else:
-                entry_iter = self.tree_store.iter_next(entry_iter)
-
+        while entry_iter is not None:
             if self.tree_store.get_value(entry_iter, 0) == entry:
                 return entry_iter
+            entry_iter = self.tree_store.iter_next(entry_iter)
         return None
 
     def find_item(self, item):
@@ -113,14 +124,10 @@ class LRGtk:
                 return tree_iter
         return None
 
-    def exit(self, widget, event):
-        gtk.main_quit()
-        exit(0)
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                         datefmt='%H:%M:%S',
                         stream=sys.stderr)
     lrgtk = LRGtk()
-    lrgtk.main()
+    gtk.main()
